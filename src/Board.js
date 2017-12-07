4f6b81681a330700d2612380e4804ef064f4978a
acenine <acenine@gmx.com>
2017-12-06 16:52:02 -0800

    Completed Board helper functions

--- src/Board.js
+++ src/Board.js
@@ -9,7 +9,7 @@
     initialize: function (params) {
       if (_.isUndefined(params) || _.isNull(params)) {
         console.log('Good guess! But to use the Board() constructor, you must pass it an argument in one of the following formats:');
-        console.log('\t1. An object. To create an empty board of size n:\n\t\t{n: %c<num>%c} - Where %c<num> %cis the dimension of the (empty) board you wish to instantiate\n\t\t%cEXAMPLE: var board = new Board({n:5})', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
+        console.log('\t1. An object. To create an empty board of size n:\n\t\t{n: %c<num>%c} - Where %c<num> %cis the dimension of the (empty) board you wish to instantiate\n\t\t%cEXAMPLE: var myboard = new Board({n:5})', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
         console.log('\t2. An array of arrays (a matrix). To create a populated board of size n:\n\t\t[ [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...] ] - Where each %c<val>%c is whatever value you want at that location on the board\n\t\t%cEXAMPLE: var board = new Board([[1,0,0],[0,1,0],[0,0,1]])', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
       } else if (params.hasOwnProperty('n')) {
         this.set(makeEmptyMatrix(this.get('n')));
@@ -79,11 +79,24 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+
+      var rowToSearch = this.attributes[rowIndex];
+      for (var i = 0; i < rowToSearch.length; i++) {
+        var total = rowToSearch.reduce( (acc, val) => acc + val);
+        if (total > 1) {
+          return true;
+        }
+      }
+      return false;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
+      for (var i = 0; i < this.get('n'); i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
       return false; // fixme
     },
 
@@ -94,11 +107,30 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
+      var colHasPiece = false;
+      for (var i = 0; i < this.get('n'); i++) {
+        var currentRow = this.attributes[i];
+        var currentColItem = currentRow[colIndex];
+        //if colhasPiece is false && currentColItem is one, we change the colhasPiece to true;
+        //if colhasPiece is true && currenColItem is one, then we have a conflict
+        if (currentColItem === 1) {
+          if(colHasPiece === true) {
+            return true;
+          } else {
+            colHasPiece = true;
+          }
+        }
+      }
       return false; // fixme
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      for (var i = 0; i < this.get('n'); i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
       return false; // fixme
     },
 
@@ -108,12 +140,50 @@
     // --------------------------------------------------------------
     //
     // test if a specific major diagonal on this board contains a conflict
-    hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
+    hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {  // we are given the column index, and this is only on the first row?
+    //   var majorDiagHasPiece = false;
+    //   var movingDownDiagonal = false;
+    //   for (var i = 0; i < this.get('n'); i++) {
+    //     var currentRow = this.attributes[i];
+    //     var currentColItem = currentRow[majorDiagonalColumnIndexAtFirstRow]; // ifmovingdown digonal then plus one otherwise keep
+    //     if (currentColItem === 1) {
+    //       // we have found a piece
+    //       // now we have to look down the iagonal from this point
+    //       // if we find another
+    //     }
+    //   }
+    //   return false; // fixme
+    // },
+      var currentColIndex = majorDiagonalColumnIndexAtFirstRow
+      var majorDiagHasPiece = false;
+      for (var i = 0; i < this.get('n'); i++) {
+        var currentRow = this.attributes[i];
+        var currentColItem = currentRow[currentColIndex]; // ifmovingdown digonal then plus one otherwise keep
+        if (majorDiagHasPiece === false) {
+          // we have found a piece
+          // now we have to look down the iagonal from this point
+          // if we find another
+
+          if (currentColItem === 1) {
+              majorDiagHasPiece = true;
+          }
+        }
+        else {
+          currentColIndex ++;
+          if (currentRow[currentColIndex] === 1){
+            return true;
+          }
+        }
+      }
       return false; // fixme
     },
-
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
+      for (var i = 0; i < this.get('n'); i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
       return false; // fixme
     },
 
@@ -124,11 +194,37 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
+      var currentColIndex = minorDiagonalColumnIndexAtFirstRow
+      var minorDiagHasPiece = false;
+      for (var i = 0; i < this.get('n'); i++) {
+        var currentRow = this.attributes[i];
+        var currentColItem = currentRow[currentColIndex]; // ifmovingdown digonal then plus one otherwise keep
+        if (minorDiagHasPiece === false) {
+          // we have found a piece
+          // now we have to look down the iagonal from this point
+          // if we find another
+
+          if (currentColItem === 1) {
+              minorDiagHasPiece = true;
+          }
+        }
+        else {
+          currentColIndex --;
+          if (currentRow[currentColIndex] === 1){
+            return true;
+          }
+        }
+      }
       return false; // fixme
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
+      for (var i = 0; i < this.get('n'); i++) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
       return false; // fixme
     }
 
